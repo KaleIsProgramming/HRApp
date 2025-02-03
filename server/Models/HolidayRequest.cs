f1385ac649e0a26cad9c9c57da130cbd35b8aaa5
Piotr Kostecki <95875856+KaleIsProgramming@users.noreply.github.com>
2025-02-03 18:57:23 +0100

    Feat: Added First class to the API
    Feat: Added First class to the API called HolidayRequest.

---
+++ server/Models/HolidayRequest.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveRequestApi.Models
+{
+    public enum HolidayType
+    {
+        UrlopOkolicznosciowy,
+        UrlopWypoczynkowy,
+        UrlopNaZadanie,
+        OpiekaNadDzieckiemDni,
+        OpiekaNadDzieckiemGodziny
+    }
+
+    public class HolidayRequest
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public HolidayType HolidayType { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        public string? OccasionalType { get; set; }
+        public string? PersonName { get; set; }
+
+        public string? SapNumber { get; set; }
+
+        public TimeSpan? StartTime { get; set; }
+        public TimeSpan? EndTime { get; set; }
+
+        public string? Comment { get; set; }
+    }
+}
