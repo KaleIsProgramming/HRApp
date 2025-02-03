bcd47cba0f9e0fa1f9c182aca104d8f60fa85a78
Piotr Kostecki <95875856+KaleIsProgramming@users.noreply.github.com>
2025-02-03 19:29:48 +0100

    Feat: Made validation on  server side and made endpoint save data of Holiday
    Feat: Made validation on  server side and made endpoint save data of Holiday

--- server/Controllers/HolidayRequestsController.cs
+++ server/Controllers/HolidayRequestsController.cs
@@ -17,8 +17,34 @@ namespace HolidayRequestApi.Controllers
 
         [HttpPost]
         public async Task<IActionResult> CreateLeaveRequest([FromBody] HolidayRequest dto)
-        {
+        { 
 
+            if (dto.StartDate > dto.EndDate)
+            {
+                return BadRequest(new { message = "Data początkowa nie może być późniejsza niż data końcowa." });
+            }
+
+
+            switch (dto.HolidayType)
+            {
+                case HolidayType.UrlopOkolicznosciowy:
+                    if (string.IsNullOrWhiteSpace(dto.OccasionalType))
+                        return BadRequest(new { message = "Należy podać rodzaj urlopu okolicznościowego." });
+                    if (string.IsNullOrWhiteSpace(dto.PersonName))
+                        return BadRequest(new { message = "Należy podać imię i nazwisko osoby, której dotyczy wniosek." });
+                    break;
+                case HolidayType.UrlopNaZadanie:
+                    if (string.IsNullOrWhiteSpace(dto.SapNumber))
+                        return BadRequest(new { message = "Numer SAP jest wymagany dla urlopu na żądanie." });
+                    break;
+                case HolidayType.OpiekaNadDzieckiemGodziny:
+                    if (!dto.StartTime.HasValue || !dto.EndTime.HasValue)
+                        return BadRequest(new { message = "Należy podać godzinę początkową i końcową." });
+                    if (dto.StartTime >= dto.EndTime)
+                        return BadRequest(new { message = "Godzina początkowa musi być wcześniejsza niż godzina końcowa." });
+                    break;
+
+            }
             _context.HolidayRequests.Add(dto);
             await _context.SaveChangesAsync();
 
